a082ebeba2720d4b1f71ca4562602f8683a07b2e
Renato Soffiatto <renato.soffiatto@grupozap.com>
2021-08-12 14:43:43 -0300

    Renato e Layonn

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/dnogueir/golang-dojo
 
-go 1.16
+go 1.13
--- main.go
+++ main.go
@@ -1,18 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	var list []int
-	var sub_list []int
+	var wg sync.WaitGroup
 
 	list = generate_slice(800)
 
-	sub_list = sub_slice(list, 0, 100)
-	
-	
-	fmt.Println(sum_values(sub_list))
-	fmt.Println(sum_values(list))
+	c := make(chan int)
+
+	wg.Add(8)
+	go func() {
+		c <- sum_values(sub_slice(list, 0, 100))
+		wg.Done()
+	}()
+	go func() {
+		c <- sum_values(sub_slice(list, 100, 200))
+		wg.Done()
+	}()
+	go func() {
+		c <- sum_values(sub_slice(list, 200, 300))
+		wg.Done()
+	}()
+	go func() {
+		c <- sum_values(sub_slice(list, 300, 400))
+		wg.Done()
+	}()
+	go func() {
+		c <- sum_values(sub_slice(list, 400, 500))
+		wg.Done()
+	}()
+	go func() {
+		c <- sum_values(sub_slice(list, 500, 600))
+		wg.Done()
+	}()
+	go func() {
+		c <- sum_values(sub_slice(list, 600, 700))
+		wg.Done()
+	}()
+	go func() {
+		c <- sum_values(sub_slice(list, 700, 800))
+		wg.Done()
+	}()
+
+	sum := 0
+	for partial_sum := range c {
+		sum += partial_sum
+	}
+
+	fmt.Println(sum)
+	wg.Wait()
 }
 
 func sum_values(values []int) int {
@@ -38,6 +79,3 @@ func sub_slice(slice []int, start int, end int) []int {
 	}
 	return sub_slice
 }
-
-
-
