45e2cff0c10f1faa21aa891dd622ed766c6173c6
Guilherme Miranda <guilhermealvesmiranda99@gmail.com>
2021-08-12 14:31:29 -0300

    chore: add sub_slice

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main() {
 	var list []int
+	var sub_list []int
 
-	list = append(list, 1, 2, 3)
+	list = generate_slice(800)
 
+	sub_list = sub_slice(list, 0, 100)
+	
+	
+	fmt.Println(sum_values(sub_list))
 	fmt.Println(sum_values(list))
 }
 
@@ -17,3 +22,22 @@ func sum_values(values []int) int {
 	}
 	return sum
 }
+
+func generate_slice(value int) []int {
+	var list []int
+	for i := 0; i < value; i++ {
+		list = append(list, i)
+	}
+	return list
+}
+
+func sub_slice(slice []int, start int, end int) []int {
+	var sub_slice []int
+	for i := start; i < end; i++ {
+		sub_slice = append(sub_slice, slice[i])
+	}
+	return sub_slice
+}
+
+
+
